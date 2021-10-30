e34ecda20ecc89bce7d58c2e396320562b6d150d
Emily Xiong <xiongemi@gmail.com>
2021-10-30 16:02:15 -0400

    initial commit

---
+++ apps/studio-ghibli-search-engine-web/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'studio-ghibli-search-engine-web',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/studio-ghibli-search-engine-web',
+};
