937a7984a11b2908566cf799d6974b8d9982c9be
Emily Xiong <xiongemi@gmail.com>
2021-11-24 23:40:45 -0500

    add react-native-vector-icons

--- apps/studio-ghibli-search-engine-mobile/src/app/search/search.tsx
+++ apps/studio-ghibli-search-engine-mobile/src/app/search/search.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
+import Logo from '../../assets/logo.svg';
 
 /* eslint-disable-next-line */
 export interface SearchProps {}
 
 export function Search(props: SearchProps) {
+  const [text, setText] = React.useState('');
   return (
-    <View testID="search">
-      <Text>Welcome to search!</Text>
-    </View>
+    <ScrollView>
+      <Logo width={100} height={100} />
+      <TextInput
+        label="Any film or character"
+        value={text}
+        onChangeText={(text) => setText(text)}
+      />
+      <Button
+        icon="magnify"
+        mode="contained"
+        onPress={() => console.log('Pressed')}
+      >
+        Press me
+      </Button>
+    </ScrollView>
   );
 }
 
