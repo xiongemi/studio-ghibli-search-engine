ba351ccdd16cccf87d388b59fe294525b962d724
Emily Xiong <xiongemi@gmail.com>
2022-04-01 13:03:48 -0400

    update storybook

--- .storybook/story-loader.js
+++ .storybook/story-loader.js
@@ -4,14 +4,14 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
-  require('./apps/studio-ghibli-search-engine-mobile/src/app/App.stories');
-  require('./apps/studio-ghibli-search-engine-mobile/src/app/film/film.stories');
-  require('./apps/studio-ghibli-search-engine-mobile/src/app/results/film-list-item/film-list-item.stories');
-  require('./apps/studio-ghibli-search-engine-mobile/src/app/results/people-list-item/people-list-item.stories');
-  require('./apps/studio-ghibli-search-engine-mobile/src/app/results/result-list-item/result-list-item.stories');
-  require('./apps/studio-ghibli-search-engine-mobile/src/app/search/search.stories');
-  require('./apps/studio-ghibli-search-engine-mobile/src/app/shared/film-card/film-card.stories');
-  require('./apps/studio-ghibli-search-engine-mobile/src/app/shared/loading/loading.stories');
+  require('../apps/studio-ghibli-search-engine-mobile/src/app/App.stories');
+  require('../apps/studio-ghibli-search-engine-mobile/src/app/film/film.stories');
+  require('../apps/studio-ghibli-search-engine-mobile/src/app/results/film-list-item/film-list-item.stories');
+  require('../apps/studio-ghibli-search-engine-mobile/src/app/results/people-list-item/people-list-item.stories');
+  require('../apps/studio-ghibli-search-engine-mobile/src/app/results/result-list-item/result-list-item.stories');
+  require('../apps/studio-ghibli-search-engine-mobile/src/app/search/search.stories');
+  require('../apps/studio-ghibli-search-engine-mobile/src/app/shared/film-card/film-card.stories');
+  require('../apps/studio-ghibli-search-engine-mobile/src/app/shared/loading/loading.stories');
 }
 
 const stories = [
