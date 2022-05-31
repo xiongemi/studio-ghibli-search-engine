84d2732da5b929ac17a813dc45d7ede627069d4b
Emily Xiong <xiongemi@gmail.com>
2022-05-30 22:17:18 -0400

    use https for mobile app

--- libs/services/src/lib/search/search.service.ts
+++ libs/services/src/lib/search/search.service.ts
@@ -3,8 +3,8 @@ import { PeopleResponse } from '../models/people-response.interface';
 import { getEnv } from '../utils/get-env';
 
 const isDevelopment = process.env.NODE_ENV === 'development';
-const baseUrl =
-  (isDevelopment ? 'http' : 'https') + getEnv('NX_REQUEST_BASE_URL');
+const useHttps = isDevelopment || getEnv('NX_APP_ENV') === 'MOBILE';
+const baseUrl = (useHttps ? 'http' : 'https') + getEnv('NX_REQUEST_BASE_URL');
 
 export async function getFilms(): Promise<FilmResponse[]> {
   const response: Response = await fetch(baseUrl + '/films', { method: 'GET' });
