073fad4c002db229b8524aaa3798099c60c50586
Emily Xiong <xiongemi@gmail.com>
2022-06-21 10:43:47 -0400

    upgrade nx to 14.3.6

--- apps/studio-ghibli-search-engine-mobile/jest.config.ts
+++ apps/studio-ghibli-search-engine-mobile/jest.config.ts
@@ -2,7 +2,6 @@
 export default {
   displayName: 'studio-ghibli-search-engine-mobile',
   preset: 'react-native',
-  testRunner: 'jest-jasmine2',
   resolver: '@nrwl/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html', 'tsx', 'jsx'],
   setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
@@ -13,9 +12,15 @@ export default {
     '.svg': '@nrwl/react-native/plugins/jest/svg-mock',
   },
   transform: {
+    '^.+\\.(js|ts|tsx)$': './preprocessor.js',
     '^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp)$': require.resolve(
       'react-native/jest/assetFileTransformer.js'
     ),
+  },
+  globals: {
+    'ts-jest': {
+      babelConfig: require('./babel.config.json'),
+    },
   },
   testEnvironment: 'node',
 };
