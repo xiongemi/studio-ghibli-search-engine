48f5c6fd00e1668b1fa1655c5f69de3006ff8b39
Emily Xiong <xiongemi@gmail.com>
2022-07-17 18:11:01 -0400

    upgrade nx to 14.4.3

--- apps/studio-ghibli-search-engine-mobile-e2e/src/search.spec.ts
+++ apps/studio-ghibli-search-engine-mobile-e2e/src/search.spec.ts
@@ -1,5 +1,7 @@
 import { device, element, by, expect } from 'detox';
 
+const pageTimeout = 10000;
+
 describe('Search', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
@@ -8,18 +10,18 @@ describe('Search', () => {
   it('should show a list of results and go to film details', async () => {
     await waitFor(element(by.id('search-page')))
       .toBeVisible()
-      .withTimeout(5000);
+      .withTimeout(pageTimeout);
     await expect(element(by.id('search-input'))).toBeVisible();
     await element(by.id('search-input')).clearText();
     await element(by.id('search-input')).typeText('totoro');
     await element(by.id('search-button')).tap();
     await waitFor(element(by.id('results-page')))
       .toBeVisible()
-      .withTimeout(5000);
+      .withTimeout(pageTimeout);
     await element(by.id('result-list-item')).atIndex(0).tap();
     await waitFor(element(by.id('film-page')))
       .toBeVisible()
-      .withTimeout(5000);
+      .withTimeout(pageTimeout);
     await expect(element(by.id('title'))).toBeVisible();
     await expect(element(by.id('title'))).toHaveText(
       'My Neighbor Totoro'
