ad07d9b2b4827d9fae671e9673b53398f80e8bc3
Emily Xiong <xiongemi@gmail.com>
2023-04-21 11:12:37 -0400

    update nx to 16 beta

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,16 @@
 module.exports = {
-  // uncomment the property below if you want to apply some webpack config globally
-  // webpackFinal: async (config, { configType }) => {
-  //   // Make whatever fine-grained changes you need that should apply to all storybook configs
-  //   // Return the altered config
-  //   return config;
-  // },
-};
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  addons: ["@storybook/addon-mdx-gfm"],
+  docs: {
+    autodocs: true
+  }
+} // uncomment the property below if you want to apply some webpack config globally
+// webpackFinal: async (config, { configType }) => {
+//   // Make whatever fine-grained changes you need that should apply to all storybook configs
+//   // Return the altered config
+//   return config;
+// },
+;
